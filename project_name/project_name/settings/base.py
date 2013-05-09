014f3f847c6a9160b8e403a72baa4f94b2f57562
Anton Agestam <msn@antonagestam.se>
2013-05-09 21:15:20 +0200

    time-zone and internationalization/localization settings

--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -90,7 +90,7 @@ CELERY_REDIS_MAX_CONNECTIONS = 256
 
 ########## GENERAL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#time-zone
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = 'Europe/Stockholm'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = 'en-us'
@@ -99,10 +99,10 @@ LANGUAGE_CODE = 'en-us'
 SITE_ID = 1
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
-USE_I18N = True
+USE_I18N = False
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
-USE_L10N = True
+USE_L10N = False
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
 USE_TZ = True
