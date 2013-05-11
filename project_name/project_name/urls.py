62b18ece879fb622af0ddf6044bf7a06e7fc51e3
Anton Agestam <msn@antonagestam.se>
2013-05-11 11:05:38 +0200

    url for serving static files

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.views.static import serve
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,4 +19,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    # Static files
+    url(r'^static/(?P<path>.*)$', serve,
+        kwargs={'document_root':settings.STATIC_ROOT})
 )
